b447dad7b0ef245badfaad4f86ee62425fa5af82
ntsebeng phaahla <ntsebengphaahla@gmail.com>
2024-08-12 15:56:10 +0200

    created project and folder structure

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Venis aesthetica</title>
+    <meta name="Venis aesthetica - A luxury mobile spar" content="A luxury mobile spar">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="favicon.ico"> <!-- Favicon -->
+	
+	<!--Bootstrap-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	
+	<!--Alegreya font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Roboto&family=Rubik:wght@700&display=swap" rel="stylesheet">	
+	
+	<!--Playfair font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto&family=Rubik:wght@700&display=swap" rel="stylesheet">	
+	
+</head>
+
+
+
+<body>
+ 	<nav class="navbar navbar-expand-lg">
+	  <div class="container">
+		<a class="navbar-brand" href="index.php"><img src="images/logo-new.jpg"></a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+			<li class="nav-item">
+			  <a class="nav-link active" aria-current="page" href="#about-us">About Us</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#treatments">Treatments</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#contact-us">Contact Us</a>
+			</li>
+<!--			<li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+				Dropdown
+			  </a>
+			  <ul class="dropdown-menu">
+				<li><a class="dropdown-item" href="#">Action</a></li>
+				<li><a class="dropdown-item" href="#">Another action</a></li>
+				<li><hr class="dropdown-divider"></li>
+				<li><a class="dropdown-item" href="#">Something else here</a></li>
+			  </ul>
+			</li>-->
+<!--			<li class="nav-item">
+			  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+			</li>-->
+		  </ul>
+<!--		  <form class="d-flex" role="search">
+			<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success" type="submit">Search</button>
+		  </form>-->
+		</div>
+	  </div>
+	</nav>
+
+
+
+
+	<!--banner-->
+	<div class="banner">
+	  <div class="container">
+		  <div class="row">
+			  <div class="col-lg-12">
+				  
+				<h1 class="color-brown">Venis Aesthetica - A luxury mobile spa</h1>
+				<h3 class="color-brown playfair-txt">We bring the essence of tranquility and the pinnacle of pampering directly to you</h3>
+				
+			  </div>
+		  </div>
+	  </div>
+	</div>
+	<!--banner-->	  
+
+
+
+
+
+	<!--About us start-->
+	<div class="container">
+	  <div class="row">
+		  <div class="col-lg-12">
+			  <div class="about-us body-padding">
+				<h2><a name="about-us"></a>Welcome to Venis Aesthetica Mobile Spa</h2>
+			
+				<p class="mb-0">Wellcome to Venus Aesthetica. Embark on a journey of relaxation and rejuvenation at Venus Aesthetica, your mobile spa oasis. We bring the essence of tranquility and the pinnacle of pampering directly to you, transforming your space into a haven of therapeutic bliss. We specialize in a variety of massages, each tailored to meet your individual needs and melt away the stresses of daily life. With Venus Aesthetica, luxury and comfort are not destinations; they are experiences we deliver to your doorstep. Indulge in the ultimate convenience of our mobile massage services and let us nurture your well-being on your schedule, wherever you choose.</p>
+			  </div>
+		  </div>
+	  </div>
+	</div>
+	<!--About us end-->
+
+
+
+	<!--Treatments start-->
+	<div class="body-padding pt-0">
+		<div class="container">
+		  <div class="row">
+			  <div class="col-lg-12">
+				<h2><a name="treatments"></a>Treatments</h2>
+			  </div>
+			  <div class="col-lg-4 col-md-6">
+				  <div class="treatment-body">
+					<img src="images/treatments/back-neck-head-massage.jpg" class="treatment-img">
+					<h3>Back, neck and head massage</h3>
+					<p>Swedish massage - is a gentle, relaxing, and rejuvenating massage that promote relaxation, ease muscle tension, and improve circulation, without a special oil but using a normal seed oil.</p>
+				  </div>
+			  </div>
+			  <div class="col-lg-4 col-md-6">
+				  <div class="treatment-body">
+					<img src="images/treatments/swedish-massage.jpg" class="treatment-img">
+					<h3>Swedish massage</h3>
+					<p>Swedish massage - is a gentle, relaxing, and rejuvenating massage that promote relaxation, ease muscle tension, and improve circulation, without a special oil but using a normal seed oil.</p>
+				  </div>
+			  </div>
+			  <div class="col-lg-4 col-md-6">
+				  <div class="treatment-body">
+					<img src="images/treatments/deep-tissue-massage.jpg" class="treatment-img">
+					<h3>Deep tissue massage</h3>
+					<p>Swedish massage - is a gentle, relaxing, and rejuvenating massage that promote relaxation, ease muscle tension, and improve circulation, without a special oil but using a normal seed oil.</p>
+				  </div>
+			  </div>
+			  <div class="col-lg-4 col-md-6">
+				  <div class="treatment-body">
+					<img src="images/treatments/hot-stone-massage.jpg" class="treatment-img">
+					<h3>Hot stone massage</h3>
+					<p>Swedish massage - is a gentle, relaxing, and rejuvenating massage that promote relaxation, ease muscle tension, and improve circulation, without a special oil but using a normal seed oil.</p>
+				  </div>
+			  </div>
+			  <div class="col-lg-4 col-md-6">
+				  <div class="treatment-body">
+					<img src="images/treatments/aroma-therapy-massage.jpg" class="treatment-img">
+					<h3>Aroma therapy massage</h3>
+					<p>Swedish massage - is a gentle, relaxing, and rejuvenating massage that promote relaxation, ease muscle tension, and improve circulation, without a special oil but using a normal seed oil.</p>
+				  </div>
+			  </div>
+		  </div>
+		</div>
+	</div>
+	<!--Treatments end-->
+
+
+
+
+	<!--Treatments start-->
+	<div class="body-padding pt-0">
+		<div class="container">
+		  <div class="row">
+				<div class="col-lg-12">
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>	
+	
+	
+	<!--C:\xampp\htdocs\projects\websites\venis aesthetica-->
+</body>
+</html>
