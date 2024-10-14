5064307f268c852eb8a17e51ca0be8c4937f7a75
Ntsebeng <ntsebengphaahla@gmail.com>
2024-10-14 16:59:05 +0200

    Added new files to repo

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+	
+    <!-- <link rel="stylesheet" href="css/styles.scss"> -->
+    <link rel="icon" href="favicon.ico"> <!-- Favicon -->
+	
+	<!--Bootstrap-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	
+	<!--Alegreya font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Roboto&family=Rubik:wght@700&display=swap" rel="stylesheet">	
+	
+	<!--Playfair font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto&family=Rubik:wght@700&display=swap" rel="stylesheet">	
+    <link rel="stylesheet" href="css/styles.css">
--- index.php
+++ index.php
@@ -6,22 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Venis aesthetica</title>
     <meta name="Venis aesthetica - A luxury mobile spar" content="A luxury mobile spar">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="icon" href="favicon.ico"> <!-- Favicon -->
 	
-	<!--Bootstrap-->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	
-	<!--Alegreya font-->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Roboto&family=Rubik:wght@700&display=swap" rel="stylesheet">	
-	
-	<!--Playfair font-->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto&family=Rubik:wght@700&display=swap" rel="stylesheet">	
-	
+<?php include '<includes/head.php' ?>	
 </head>
 
 
@@ -29,11 +15,13 @@
 <body>
  	<nav class="navbar navbar-expand-lg">
 	  <div class="container">
-		<a class="navbar-brand" href="index.php"><img src="images/logo-new.jpg"></a>
+		<a class="navbar-brand" href="index.php"><img src="images/logo-white.png"></a>
+
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		  <span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+		<div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
 		  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 			<li class="nav-item">
 			  <a class="nav-link active" aria-current="page" href="#about-us">About Us</a>
@@ -42,7 +30,7 @@
 			  <a class="nav-link" href="#treatments">Treatments</a>
 			</li>
 			<li class="nav-item">
-			  <a class="nav-link" href="#contact-us">Contact Us</a>
+			  <a class="nav-link" href="#contact-us">Contact Us</a>	
 			</li>
 <!--			<li class="nav-item dropdown">
 			  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -75,10 +63,12 @@
 	  <div class="container">
 		  <div class="row">
 			  <div class="col-lg-12">
-				  
-				<h1 class="color-brown">Venis Aesthetica - A luxury mobile spa</h1>
-				<h3 class="color-brown playfair-txt">We bring the essence of tranquility and the pinnacle of pampering directly to you</h3>
-				
+				<div class="banner-header">
+					
+					<h1 class="">Venis Aesthetica - A luxury mobile spa</h1>
+					<h3 class=" playfair-txt">We bring the essence of tranquility and the pinnacle of pampering directly to you</h3>
+					
+				</div>
 			  </div>
 		  </div>
 	  </div>
@@ -94,7 +84,7 @@
 	  <div class="row">
 		  <div class="col-lg-12">
 			  <div class="about-us body-padding">
-				<h2><a name="about-us"></a>Welcome to Venis Aesthetica Mobile Spa</h2>
+				<h2 class=""><a name="about-us"></a>Welcome to Venis Aesthetica Mobile Spa</h2>
 			
 				<p class="mb-0">Wellcome to Venus Aesthetica. Embark on a journey of relaxation and rejuvenation at Venus Aesthetica, your mobile spa oasis. We bring the essence of tranquility and the pinnacle of pampering directly to you, transforming your space into a haven of therapeutic bliss. We specialize in a variety of massages, each tailored to meet your individual needs and melt away the stresses of daily life. With Venus Aesthetica, luxury and comfort are not destinations; they are experiences we deliver to your doorstep. Indulge in the ultimate convenience of our mobile massage services and let us nurture your well-being on your schedule, wherever you choose.</p>
 			  </div>
@@ -110,7 +100,7 @@
 		<div class="container">
 		  <div class="row">
 			  <div class="col-lg-12">
-				<h2><a name="treatments"></a>Treatments</h2>
+				<h2 class=""><a name="treatments"></a>Treatments</h2>
 			  </div>
 			  <div class="col-lg-4 col-md-6">
 				  <div class="treatment-body">
@@ -173,5 +163,21 @@
 	
 	
 	<!--C:\xampp\htdocs\projects\websites\venis aesthetica-->
+
+<script>
+
+document.addEventListener('scroll', () => {
+	const nav = document.querySelector ('nav');
+
+	if (window.scrollY > 0){
+		nav.classList.add('scrolled');
+	}else{
+		nav.classList.remove('scrolled');
+	}
+});
+
+</script>
+
+
 </body>
 </html>
